36b6df5a61a4f0c420db392bda71827477ac67db
AlmazWh <139867461+AlmazWh@users.noreply.github.com>
2023-09-06 09:45:14 +0300

    robot based on Open CM 9.04 controller
    Unfortunately, I lost the file with the code that I uploaded to the controller

---
+++ Control.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice
+
+app = QtWidgets.QApplication([])
+ui = uic.loadUi("Desi.ui")
+ui.setWindowTitle("ControlDI")
+
+serial = QSerialPort()
+serial.setBaudRate(115200)
+portList = []
+ports = QSerialPortInfo().availablePorts()
+for port in ports:
+    portList.append(port.portName())
+print(portList)
+
+ui.show()
+app.exec()
